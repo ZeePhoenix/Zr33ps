baeb574649530c6cff4471a4b893347968efb89e
Zac <zcupec@gmail.com>
2022-03-13 10:43:42 -0400

    implemented switch for finishing plans

--- src/room/base/tasks/finishPlans.ts
+++ src/room/base/tasks/finishPlans.ts
@@ -5,18 +5,25 @@ const finishPlans: TaskRunner = {
 	init:(context: TaskProgressContext, room: Room) => {
 		const flags = room.find(FIND_FLAGS).filter(f => f.name.includes('Site'));
 		flags.forEach(flag => {
-			if(flag.name.includes('Storage')){
-				//@ts-ignore
-				if(room.controller?.level >= 4){
-					room.createConstructionSite(flag.pos, STRUCTURE_STORAGE);
-				}
-			} else if (flag.name.includes('Tower')){
-				room.createConstructionSite(flag.pos, STRUCTURE_TOWER);
-			} else if (flag.name.includes('Road')){
-				room.createConstructionSite(flag.pos, STRUCTURE_ROAD);
-			} else {
-				// TODO make this a switch for all BuildableStructureConstants
-				throw new Error(`could not construct ${flag.name}`);
+			let structString = flag.name.substring(0, flag.name.indexOf('*'));
+			let level = room.controller!.level;
+			switch(structString){
+				case 'StorageSite':
+					if (level >= 4){
+						room.createConstructionSite(flag.pos, STRUCTURE_STORAGE);
+						flag.remove();
+					}
+					break;
+				case 'TowerSite':
+					if (level >= 3){
+						room.createConstructionSite(flag.pos, STRUCTURE_TOWER);
+						flag.remove();
+					}
+					break;
+				case 'RoadSite':
+					room.createConstructionSite(flag.pos, STRUCTURE_ROAD);
+					flag.remove();
+					break;
 			}
 		});
 	},
