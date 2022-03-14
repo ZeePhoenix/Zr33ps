d95de0c5c50428074eeddc112e1fbf742d8115d7
Zac <zcupec@gmail.com>
2022-03-14 11:01:19 -0400

    Adding building roads to the spawn

--- src/room/base/tasks/buildRoads.ts
+++ src/room/base/tasks/buildRoads.ts
@@ -4,8 +4,18 @@ const buildRoadsToSources: TaskRunner ={
 	delayCheck: true,
 	init: (context:TaskProgressContext, room:Room) => {
 		const sources = room.find(FIND_SOURCES);
+		// build roads to controller
 		sources.forEach(source => {
 			const pathSteps = source.pos.findPathTo(room.controller!.pos);
+			pathSteps.forEach(step => {
+				if (room.createConstructionSite(step.x,step.y,STRUCTURE_ROAD) !== OK ){
+					console.log(`Error creating road at (${step.x,step.y})`);
+				}
+			});
+		});
+		sources.forEach(source => {
+			const spawn = room.find(FIND_MY_SPAWNS)[0];
+			const pathSteps = source.pos.findPathTo(spawn.pos);
 			pathSteps.forEach(step => {
 				if (room.createConstructionSite(step.x,step.y,STRUCTURE_ROAD) !== OK ){
 					console.log(`Error creating road at (${step.x,step.y})`);
